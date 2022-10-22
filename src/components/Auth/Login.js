aadc2b3bde967e28a6b2fc74d918c7b1c67bcad4
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-10-22 21:48:47 +0600

    dsafas

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 import { BiLogInCircle } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import "../../styles/login.css";
 
 const Login = () => {
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const form = e.target;
@@ -12,6 +20,19 @@ const Login = () => {
     console.log(email, password);
   };
 
+  const handleEmailChange = (e) => {
+    setUserInfo({ ...userInfo, email: e.target.value });
+  };
+  const handlePasswordChange = (e) => {
+    const password = e.target.value;
+    if (password) {
+      setError("must be at last 6 character");
+      setUserInfo({ ...userInfo, password: e.target.value });
+    } else {
+      setError("");
+      setUserInfo({ ...userInfo, password: e.target.value });
+    }
+  };
   return (
     <div className="login-container">
       <div className="login-title">
@@ -19,8 +40,27 @@ const Login = () => {
         <BiLogInCircle />
       </div>
       <form className="login-form" onSubmit={handleSubmit}>
-        <input type="text" name="email" placeholder="Your Email" />
-        <input type="password" name="password" placeholder="password" />
+        <input
+          type="text"
+          name="email"
+          placeholder="Your Email"
+          value={userInfo.email}
+          onChange={handleEmailChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={userInfo.password}
+          onChange={handlePasswordChange}
+        />
+        {userInfo.password.length > 6 ? (
+          <>{}</>
+        ) : (
+          <>
+            <p className="error-message">{error}</p>
+          </>
+        )}
         <button>Login</button>
 
         <p>
